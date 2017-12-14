a79b4c703799b6e76a736a66ed277297338c8039
RyanMoven <ryanmoven@gmail.com>
2017-12-14 17:52:22 -0500

    added images for weatherResults

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -12,8 +12,22 @@ $('#end').on('click', function(){
   dataType : "jsonp",
   async: false,
   success : function(parsed_json) {
-  var temp_f = parsed_json.current_observation.icon
-  $("#weatherResultsDiv").append("<p>" + "Current temperature in " + city + " is:"  + temp_f + "</p>");
+  var condition = parsed_json.current_observation.icon
+
+  // Saving the current_observation.icon_url property
+        var imageUrl = parsed_json.current_observation.icon_url
+
+        // Creating and storing an image tag
+        var weatherImage = $("<img>");
+
+        // Setting the weatherImage src attribute to imageUrl
+        weatherImage.attr("src", imageUrl);
+        weatherImage.attr("alt", "Today's forecast");
+
+  $("#weatherResultsDiv").append("<p>" + "Current condition in " + city + "is:" + condition + "</p>");
+
+
+  $("#weatherResultsDiv").append(weatherImage);
   }
   		}); // end of Ajax call
 
