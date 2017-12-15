3f3e28beabba6215be3f590017ed97f3ac22b857
Keissy Leonardo <keissyleonardo@gmail.com>
2017-12-14 21:28:53 -0500

    past adventures table populates with firebase database and reset button

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,9 +1,111 @@
+
+
+// Initializing Firebase 
+  var config = {
+    apiKey: "AIzaSyAcG4BqNXIOen6kziVMAtUmB21__n2e7JY",
+    authDomain: "project1-group5.firebaseapp.com",
+    databaseURL: "https://project1-group5.firebaseio.com",
+    projectId: "project1-group5",
+    storageBucket: "project1-group5.appspot.com",
+    messagingSenderId: "448115174573"
+  };
+  firebase.initializeApp(config);
+
+
+var database = firebase.database();
+
+//  pulling the info inputted from the user into the database
+
+database.ref("/adventures").on("child_added", function(snapshot) {
+   
+// Variables for the past adventures table 
+   var name = snapshot.val().name;
+   var travelersTotal = snapshot.val().travelersTotal; 
+   var startingPoint = snapshot.val().startingPoint;
+   var destination = snapshot.val().destination;
+   var arrivalDate = snapshot.val().arrivalDate;
+   var tripDuration = snapshot.val().tripDuration;
+   var tableRow = $("<tr>");
+   var tableDisplay = $("<td>");
+
+
+// Displays on the past adventures the travelers name.  
+
+  tableRow.append(tableDisplay);
+  tableDisplay.text(name);
+
+  // displays on the past adventures the starting point 
+  var tableDisplay3 = $("<td>"); 
+  tableRow.append(tableDisplay3); 
+  tableDisplay3.text(startingPoint);
+
+// displays on the past adventures the destination 
+  var tableDisplay4 = $("<td>"); 
+  tableRow.append(tableDisplay4); 
+  tableDisplay4.text(destination);
+
+  // displays on the past adventures the arrival date 
+  var tableDisplay5 = $("<td>"); 
+  tableRow.append(tableDisplay5); 
+  tableDisplay5.text(arrivalDate);  
+
+// displays on the past adventures the trip duration 
+  var tableDisplay6 = $("<td>"); 
+  tableRow.append(tableDisplay6);
+  tableDisplay6.text(tripDuration);
+
+  // Displays on the past adventures the total travelers.  
+  var tableDisplay2 = $("<td>");
+  tableRow.append(tableDisplay2);
+  tableDisplay2.text(travelersTotal); 
+
+
+// displays on the past adventures the table row 
+ $("tbody").append(tableRow); 
+
+
+ },
+
+// Part of the firebase
+  function(errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  });
+
+
+// On click statement that collects the users input data. 
+$('#submit').on('click', function(event){
+ var name = $("#travelerName").val().trim();
+ console.log(name);
+   var travelersTotal = $("#numberTravelers").val().trim();
+   var startingPoint = $("#startingPoint").val().trim(); 
+   var destination = $("#destination").val().trim(); 
+   var arrivalDate = $("#arrivalDate").val().trim(); 
+   var tripDuration = $("#tripDuration").val().trim(); 
+
+
+// Lines that push data to the databse. 
+ database.ref("/adventures").push({
+         name: name, 
+         travelersTotal: travelersTotal,  
+         startingPoint: startingPoint,  
+         destination: destination,
+         arrivalDate: arrivalDate, 
+         tripDuration: tripDuration 
+
+        });
+
+}); 
+
+
+$("#resetButton").on("click", function(event) {
+    location.reload(event);
+ 
+    });
+
+
 $(document).ready(function() {
 $('#end').on('click', function(){
-
-
-
-
+  
 
 // Ajax request based on user's destination value
 	var city = $('#end').val()
