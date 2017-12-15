f5e6807473c6bbf2a64605cc17ca1f421cfc6ff3
RyanMoven <ryanmoven@gmail.com>
2017-12-14 20:44:11 -0500

    errs

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,39 +1,50 @@
 $(document).ready(function() {
-$('#end').on('click', function(){
+$('#submit').on('click', function(){
+ // this prevents form from reseting after clicking submit
 
-
-
-
-
+  event.preventDefault();
 // Ajax request based on user's destination value
-	var city = $('#end').val()
+ var userInputDestination = $("#destination").val();
+
+      console.log("The user's destination is:" + userInputDestination);
+
+      function renderImage(){
+
+        $("#weatherResultsDiv").empty();
+
+        // 
+
+      }
+
+    
+    //Create a new variable for the WEATHER API query URL search
+    var queryURL = "http://api.wunderground.com/api/4ca026adcc7248b3/geolookup/conditions/forecast/q/" + userInputDestination + ".json";
+
+    //AJAX
     $.ajax({
-  url : "http://api.wunderground.com/api/4ca026adcc7248b3/geolookup/conditions/forecast/q/" + city + ".json",
-  dataType : "jsonp",
-  async: false,
-  success : function(parsed_json) {
-  var condition = parsed_json.current_observation.icon
-
-  // Saving the current_observation.icon_url property
-        var imageUrl = parsed_json.current_observation.icon_url
-
+      url : queryURL,
+      dataType : "jsonp",
+      success : function(response) {
+        console.log(response);
+      }
+    });
+  
         // Creating and storing an image tag
         var weatherImage = $("<img>");
 
         // Setting the weatherImage src attribute to imageUrl
-        weatherImage.attr("src", imageUrl);
+        weatherImage.attr("src");
         weatherImage.attr("alt", "Today's forecast");
-
-  $("#weatherResultsDiv").append("<p>" + "Current condition in " + city + "is:" + condition + "</p>");
 
 
   $("#weatherResultsDiv").append(weatherImage);
-  }
-  		}); // end of Ajax call
-
+// this will prevent more than one image to be appended to the page
+    renderImage();
+// I NEED A RENDER BUTTON FUNCTION TO STOP IT FROM APPEDING CONTINUOUSLY 
 
 
 
 	}); // end of onclick
+
 
 }); // end of document.ready
